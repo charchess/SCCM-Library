98b82bf68b3017978a0752360a5d896a56d8d9ea
charchess <charchess@gmail.com>
2022-03-04 22:44:34 +0100

    Create test.tests.ps1

---
+++ test.tests.ps1
@@ -0,0 +1,13 @@
+BeforeAll { 
+    $IP1=[PAddress] "127.0.0.1/8"
+    $IP2=[IPAddress] "10.0.0.1/255.255.255.0"
+}
+
+Describe 'Check IPAddress class is working as expected' {
+    It 'Check Prefix to mask is working' {
+        $IP1.Mask | Should -Be "255.0.0.0"
+    }
+    It 'Check Mask to Prefix is working' {
+        $IP2.Prefix | Should -Be 24
+    }
+}
